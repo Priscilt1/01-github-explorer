83c40639513c30309277ced8590d749d3ab06dea
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-03-11 11:27:41 -0300

    Imutabilidade no React

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,11 @@
 // Aprendo o conceito de estado
 import { useState } from 'react' //importando o estado (que sao variaveis mentoradas pelo o react para renderizar quando mudar de valor)
 
-// hool -> gancho 
-
 export function Counter() {
     const [counter, setCounter] = useState(0) //a const retorna um array, trazendo dois retornos. 1 que é o counter que é o valor inicial[0], e o segundo o setCount que possibilita o valor ser alterado[1]
 
     function increment() {
-        setCounter(counter + 1)
+        setCounter(counter + 1) //criando um novo array, com um novo valor (conceito de imutabilidade)
     }
 
     return (
