0758391c451a8a10858543334887291744fb393c
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-03-11 12:03:08 -0300

    Fast Refresh no Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 //variaveis ambientes
 const isDevelopment = process.env.NODE_ENV !== 'production'
@@ -18,19 +19,28 @@ module.exports = {
         extensions: ['.js', '.jsx'],
     },
     devServer: { //automatização/servidor
-        contentBase: path.resolve(__dirname, 'public') //avisando o conteudo estatico
+        contentBase: path.resolve(__dirname, 'public'), //avisando o conteudo estatico
+        hot: true,
     },
-    plugins: [ //Injetando de forma automatizada o html com o script
-        new HtmlWebpackPlugin({ 
+    plugins: [
+        isDevelopment && new ReactRefreshWebpackPlugin(), //só vai rodar se estiver em ambiente de desenvolvimento        
+        new HtmlWebpackPlugin({ //Injetando de forma automatizada o html com o script
             template: path.resolve(__dirname, 'public', 'index.html')
         })
-    ],
+    ].filter(Boolean), //o plugin só funciona com o resultado true
     module: { //configurações de importação de arquivos
         rules: [ //regras
             {
                 test: /\.jsx$/, //para indentificar se é o arquivo js ou não pela a extensão
                 exclude: /node_modules/, //para o node nao importar para o browser o arquivo jsx. (ja que é responsabilidade do react)
-                use: 'babel-loader', //integração do babel e da webpack, convertendo o arquivo para que o browser entenda
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            isDevelopment && require.resolve('react-refresh/babel')
+                        ].filter(Boolean)
+                    }
+                }, //integração do babel e da webpack, convertendo o arquivo para que o browser entenda
             },
             {
                 test: /\.scss$/, //configuração para ler css
