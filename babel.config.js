18171b305a425add2ab12efd27b93dd402ad3f25
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-03-12 11:35:32 -0300

    TypeScript no ReactJs

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     presets: [
         '@babel/preset-env',
+        '@babel/preset-typescript',
         ['@babel/preset-react', {
             // para nao precisar importar o react
             runtime: 'automatic'
--- webpack.config.js
+++ webpack.config.js
@@ -10,13 +10,13 @@ module.exports = {
     mode: isDevelopment ? 'development' : 'production',
     devtool: isDevelopment ? 'eval-source-map' : 'source-map', //SourceMaps - O mesmo codigo da aplicação para conseguir debugar
     //com o path, permite que caminhe ate o aquivo sem colocar a barra, usando o dirname (diretorio), evitando o conflito entre barras
-    entry: path.resolve(__dirname, 'src', 'index.jsx'), //arquivo principal
+    entry: path.resolve(__dirname, 'src', 'index.tsx'), //arquivo principal
     output: { //arquivo de saida que sera mandando para o bundle
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     }, 
     resolve: {
-        extensions: ['.js', '.jsx'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
     },
     devServer: { //automatização/servidor
         contentBase: path.resolve(__dirname, 'public'), //avisando o conteudo estatico
@@ -31,7 +31,7 @@ module.exports = {
     module: { //configurações de importação de arquivos
         rules: [ //regras
             {
-                test: /\.jsx$/, //para indentificar se é o arquivo js ou não pela a extensão
+                test: /\.(j|t)sx$/, //pode aceitar js e tsc
                 exclude: /node_modules/, //para o node nao importar para o browser o arquivo jsx. (ja que é responsabilidade do react)
                 use: {
                     loader: 'babel-loader',
